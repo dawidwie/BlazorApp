370a66656ccff4b339072f95cb79da9ea91eec12
dawidwie <vlogsdawid@gmail.com>
2022-10-11 13:10:19 +0200

    Add project files.

---
+++ BlazorApp/Pages/Index.razor.cs
@@ -0,0 +1,46 @@
+﻿using BlazorApp.Models;
+
+
+
+
+namespace BlazorApp.Pages
+{
+    public partial class Index
+    {
+        private List<Contact> contacts { get; set; }
+
+        private Dictionary<string, object> MyTextboxAttributes = new Dictionary<string, object>
+    {
+        { "disabled", "disabled"},
+        { "placeholder", "Contact Name"},
+        { "type", "text"}
+    };
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            contacts = new List<Contact>
+        {
+            new Contact
+            {
+                FirstName = "Dawid",
+                LastName = "Wieczorek",
+                Email = "dawid@gmail.com"
+            },
+            new Contact
+            {
+                FirstName = "Cp",
+                LastName = "Cakelov",
+                Email = "cp@gmail.com"
+            },
+            new Contact
+            {
+                FirstName = "Patrick",
+                LastName = "Haugland",
+                Email = "patrick@gmail.com"
+            }
+        };
+            base.OnInitializedAsync();
+        }
+    }
+}
